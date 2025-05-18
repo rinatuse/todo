79e0e59aa27f56e2c6a9e43aa6dbf401ecffa8e3
rinatuse <bastanovrinat@gmail.com>
2025-05-18 22:44:36 +0300

    feat: add function

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+const firstInput = ref(22);
+const secondInput = ref("");
+function onChangeInput(event) {
+  console.log(event.target.value);
+  console.log(firstInput.value);
+
+  firstInput.value = event.target.value;
+}
+</script>
 
 <template>
-  <div>Hello</div>
+  <div>
+    <form @submit.prevent="onChangeInput">
+      <input type="text" :value="firstInput" @change="onChangeInput($event)" />
+      <input v-model="secondInput" />
+      <button>Добавить</button>
+    </form>
+  </div>
 </template>
 
 <style scoped></style>
